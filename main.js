45f0dda79c9e6d0d990862939850c9bc13727132
Anathi Anathi <anathi.gumede@codeimmersives.com>
2019-10-22 09:04:52 -0400

    reverse solution

--- main.js
+++ main.js
@@ -12,10 +12,25 @@ function crazyCase(str) {
 function ciEmailify() {
 }
 
-function exclaim() {
+function exclaim(str) {
+  let newStr = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '?') {
+      newStr += newStr + '!';
+    } else {
+      newStr += newStr + str[i];
+    }
+  }
+  return newStr;
 }
 
-function reverse() {
+function reverse(str) {
+  let newStr = '';
+  for (let i = str.length - 1; i >= 0; i--){
+    newStr += str[i];
+  }
+  return newStr;
 }
 
 function crazyCase2ReturnOfCrazyCase() {
